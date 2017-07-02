eab1c90d0cb36583ab437f33aceda402b44bf678
Patrice Mueller <mueller.patrice@gmail.com>
2017-07-02 09:45:16 +0200

    Added meat and fish and some fruits

--- lib/domain/food.dart
+++ lib/domain/food.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 
 class FoodViewModel {
 
-  FoodViewModel({this.name, this.icon});
+  FoodViewModel({this.name, this.icon, this.isSelected = false});
   final String name;
   final IconData icon;
+  bool isSelected;
 }
 
 final List<FoodViewModel> fruits = <FoodViewModel>[
@@ -22,5 +23,62 @@ final List<FoodViewModel> fruits = <FoodViewModel>[
    new FoodViewModel(name: "Blueberries",icon: Icons.work),
    new FoodViewModel(name: "Broccoli",icon: Icons.youtube_searched_for),
    new FoodViewModel(name: "Cabbage",icon: Icons.radio),
+   new FoodViewModel(name: "Carrots",icon: Icons.radio),
+   new FoodViewModel(name: "Cauliflower",icon: Icons.radio),
+   new FoodViewModel(name: "Celery",icon: Icons.radio),
+   new FoodViewModel(name: "Cherries",icon: Icons.radio),
+   new FoodViewModel(name: "Cherry tomatoes",icon: Icons.radio),
+   new FoodViewModel(name: "Chillies",icon: Icons.radio),
+   new FoodViewModel(name: "Chives",icon: Icons.radio),
+   new FoodViewModel(name: "Cilantro",icon: Icons.radio),
+   new FoodViewModel(name: "Cranberries",icon: Icons.radio),
+   new FoodViewModel(name: "Cucumber",icon: Icons.radio),
+   new FoodViewModel(name: "Dates",icon: Icons.radio),
+   new FoodViewModel(name: "Eggplant",icon: Icons.radio),
+   new FoodViewModel(name: "Fennel",icon: Icons.radio),
+   new FoodViewModel(name: "Fruit",icon: Icons.radio),
+   new FoodViewModel(name: "Garlic",icon: Icons.radio),
+   new FoodViewModel(name: "Ginger",icon: Icons.radio),
+   new FoodViewModel(name: "Grapefruit",icon: Icons.radio),
+   new FoodViewModel(name: "Grapes",icon: Icons.radio),
+   new FoodViewModel(name: "Herbs",icon: Icons.radio),
+   new FoodViewModel(name: "Kiwis",icon: Icons.radio),
+   new FoodViewModel(name: "Leek",icon: Icons.radio),
+];
+
+final List<FoodViewModel> meat = <FoodViewModel>[
+  new FoodViewModel(name: "Bacon",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Beef",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Bratwurst",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Chicken",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Chicken breast",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Cold cuts",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Gruond meat",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Ham",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Hotdog",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Lamb",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Meat",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Pork",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Prosciutto",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Salami",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Sausage",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Sausages",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Sliced beef",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Steak",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Turkey",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Turkey breast",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Veal",icon: Icons.add_shopping_cart),
+
+];
+
+final List<FoodViewModel> fish = <FoodViewModel>[
+  new FoodViewModel(name: "Anchovies",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Fish",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Lobster",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Mussles",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Oysters",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Salmon",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Shrimp",icon: Icons.add_shopping_cart),
+  new FoodViewModel(name: "Tuna",icon: Icons.add_shopping_cart),
 
 ];
